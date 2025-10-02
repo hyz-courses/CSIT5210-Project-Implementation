7771860639ad1f20e94b2567569b9858c3dcc4ed
YanzhenHuang <huangyanzhen0108@163.com>
2025-10-02 16:35:54 +0800

    feat: Add dataframe processing

--- data_process.py
+++ data_process.py
@@ -2,11 +2,21 @@ import os
 import json
 from pathlib import Path
 from typing import List, DefaultDict, Callable
+
+# Data processing
+import pandas as pd
+import csv
+
 from tqdm import tqdm
-import pickle
 from loguru import logger
 
+
+
 logger.add("logs/data_process.log", rotation="10 MB")
+
+"""
+Data Processing Infrastructures
+"""
 
 def load_jsonl_lines(
         func: Callable, 
@@ -51,12 +61,35 @@ def load_jsonl_lines(
     return dict_lines
 
 
+def extend_csv_file(dict_list: List[dict], category: str):
+    with open(f"data/grained/{category}/{category}_userint.csv") as csv_file:
+        field_names = dict_list[0].keys()
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        writer.writeheader()
+        for row in dict_list:
+            for key, value in row.items():
+                write_value = value
+                if isinstance(value, list):
+                    write_value = str(write_value)
+                writer.writerow({key: write_value})
+
+"""
+Data Graining
+"""
+
+
 def load_raw_data(category: str):
     """
     Load key contents of review and meta.
     Parameters:
-        category (str): The category of the dataset.
-                        E.g., "Video_Games", "Books", etc.
+        category (str): 
+            The category of the dataset.
+            E.g., "Video_Games", "Books", etc.
+    Returns:
+        review_list (List[dict]): 
+            A list of purified review records.
+        meta_list (List[dict]): 
+            A list of purified meta records.
     """
 
     base_path = "data/raw"
@@ -76,76 +109,102 @@ def load_raw_data(category: str):
             "parent_asin": record["parent_asin"]}, 
         jsonl_file=os.path.join(base_path, category, review_file))
 
-    print(review_list[0])
-
     return review_list, meta_list
 
 
-def load_user_interactions(
-        reviews: List[dict], 
-        metas: List[dict]):
-    
+def get_pruned_user_interaction_list(reviews: List[dict]):
     """
-    Load user interactions.
+    Get pruned list of user interactions, including:
+        - user_id: Which user
+        - timestamp: When the interaction happened
+        - parent_asin: Which item
+    Iteratively prune the list using 5-core.
+        
+    Parameters:
+        reviews (List[dict]): 
+            A list of review records.
+    Returns:
+        List[dict]: 
+            A list of pruned user interaction records.
     """
-    
-    logger.info(
-        f"[CSIT5210 Info]: Loading user interactions.")
 
-    itemid_itemtitle = {
-        meta["parent_asin"]: meta["title"] for meta in metas}
+    # which user interacted which item at which time
+    records = [{
+        "user_id": review["user_id"],
+        "timestamp": review["timestamp"],
+        "parent_asin": review["parent_asin"],
+        } for review in reviews]
 
-    user_interactions = DefaultDict(list)
-    for review in tqdm(reviews):
-        user_interactions[review["user_id"]].append({
-            "timestamp": review["timestamp"],
-            "asin": review["asin"],
-            "parent_asin": review["parent_asin"],
-            "item_title": itemid_itemtitle[review["parent_asin"]]
-        })
-
-    return user_interactions
-
-
-def save_user_interactions(
-        user_interactions: DefaultDict[str, list],
-        category: str):
-    
-    """
-    Save user interactions to .jsonl and .pkl files.
-    """
-    
-    base_path = "data/grained"
-    category_path = os.path.join(base_path, category)
-
-    if not os.path.exists(category_path):
-        os.makedirs(category_path)
+    df = pd.DataFrame(records)
 
     logger.info(
-        f"[CSIT5210 Info]: Loading user interactions to"
-        " .jsonl (for visualization).")
-    
-    with open(os.path.join(category_path, f"{category}_usritn.jsonl"), "w") as f:
-        json.dump(user_interactions, f)
-        f.close()
+        f"[CSIT5210 Info]: Before filtering, \n\n"
+        f"{len(df)} interactions, " 
+        f"{df['user_id'].nunique()} distinct users, "
+        f"{df['parent_asin'].nunique()} distinct items.\n\n")
 
-    logger.info(
-        f"[CSIT5210 Info]: Loading user interactions to pickle" 
-        " (for loading).")
+    while True:
+        user_counts = df["user_id"].value_counts()
+        item_counts = df["parent_asin"].value_counts()
+
+        # Remove users with less than 5 interactions
+        valid_users = user_counts[user_counts >= 5].index
+
+        # Remove items with less than 5 interactions
+        valid_items = item_counts[item_counts >= 5].index
+
+        df_new = df[
+            df["user_id"].isin(valid_users) &
+            df["parent_asin"].isin(valid_items)].copy()
+        
+        if len(df_new) == len(df):
+            break
+
+        df = df_new
     
-    with open(os.path.join(category_path, f"{category}_usritn.pkl"), "wb") as f:
-        pickle.dump(user_interactions, f)
-        f.close()
+    logger.info(
+        f"[CSIT5210 Info]: After filtering, \n\n"
+        f"{len(df)} interactions, " 
+        f"{df['user_id'].nunique()} distinct users, "
+        f"{df['parent_asin'].nunique()} distinct items.\n\n")
+    
+    return df
+
+
+def build_asin_itemid_map(ui_df: pd.DataFrame):
+    """
+    Obtaining a pruned user interaction list,
+    assign each item parent_asin a unique
+    numerical item id, and organize a hashmap.
+
+    Parameters:
+        ui_df (pd.DataFrame): 
+            A dataframe of user interactions.
+    Returns:
+        asin_to_itemid (dict): 
+            A dictionary mapping parent_asin to item_id.
+    """
+    
+    unique_asins = sorted(ui_df['parent_asin'].unique())
+    asin_to_itemid = {asin: i for i, asin in enumerate(unique_asins)}
+
+    return asin_to_itemid
+
+
+# def grain_user_interaction_list(
+#         ui_df: pd.DataFrame, 
+#         asin_to_itemid: dict):
+#     ui_df["item_id"] = ui_df["parent_asin"].map(asin_to_itemid)
+#     ui_df = ui_df.sort_values(["user_id", "timestamp"])
+
+#     user_itemlist = ui_df.groupby("user_id")["item_id"]
+
 
 if __name__ == "__main__":
     reviews, metas = load_raw_data(category="Video_Games")
-    uits = load_user_interactions(reviews, metas)
-    save_user_interactions(uits, category="Video_Games")
 
-    # with open("data/grained/Video_Games/Video_Games_usritn.pkl", "rb") as f:
-    #     uits = pickle.load(f)
-    #     for k, v in uits.items():
-    #         print(k)
-    #         print(v)
-    #     f.close()
-    
+    df = get_pruned_user_interaction_list(reviews)
+    map = build_asin_itemid_map(df)
+
+    for k, v in map.items():
+        print(f"{k} {v}")
